d049d94ab2c457f43479af87a974722658f3c00c
Mauro Rappa <maurorappa@aol.co.uk>
2020-03-17 20:47:49 +0000

    remove random data, start to work on real tty

--- simple.go
+++ simple.go
@@ -3,38 +3,43 @@ package simple
 // simple.go
 
 import (
-    "github.com/influxdata/telegraf"
-    "github.com/influxdata/telegraf/plugins/inputs"
-    "math/rand"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/tarm/serial"
 )
 
 type Sensor struct {
-    Input []string `toml:"sensors"`
+	Input []string `toml:"sensors"`
 }
 
 func (s *Sensor) Description() string {
-    return "Arduino plugin"
+	return "Arduino plugin"
 }
 
 func (s *Sensor) SampleConfig() string {
-    return `
+	return `
   ## an array of sensors
   sensors = ["T","H"]
 `
 }
 
 func (s *Sensor) Init() error {
-        return nil
+	return nil
 }
 
 func (s *Sensor) Gather(acc telegraf.Accumulator) error {
-    for _,s := range s.Input {
-        acc.AddFields(s, map[string]interface{}{"value": rand.Intn(100)}, nil)
-    }
-    return nil
+	for _, s := range s.Input {
+		acc.AddFields(s, map[string]interface{}{"value": 1}, nil)
+	}
+	return nil
 }
 
 func init() {
-    rand.Seed(80)
-    inputs.Add("simple", func() telegraf.Input { return &Sensor{} })
+	inputs.Add("simple", func() telegraf.Input { return &Sensor{} })
+	c := &serial.Config{Name: "/dev/tty.usbmodem14101", Baud: 9600, ReadTimeout: 1000}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Printf("cannot open serial: %s\n", err)
+		return "null"
+	}
 }
