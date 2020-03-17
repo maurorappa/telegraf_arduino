82df7ad2ab0fa7099b17fab39486621959d91205
Mauro Rappa <maurorappa@aol.co.uk>
2020-03-17 21:07:20 +0000

    ask to the real tty

--- simple.go
+++ simple.go
@@ -6,6 +6,7 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"github.com/tarm/serial"
+	"log"
 )
 
 type Sensor struct {
@@ -28,7 +29,20 @@ func (s *Sensor) Init() error {
 }
 
 func (s *Sensor) Gather(acc telegraf.Accumulator) error {
+	c := &serial.Config{Name: "/dev/tty.usbmodem14101", Baud: 9600, ReadTimeout: 1000}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Printf("cannot open serial: %s\n", err)
+		return "null"
+	}
 	for _, s := range s.Input {
+		_, err = s.Write([]byte(sensor))
+		if err != nil {
+			log.Printf("cannot write to serial: %s\n", err)
+		}
+		nbytes, failed := s.Read(buf)
+		whole_reply := string(buf)
+		log.Printf("Got %d bytes:, took %f", nbytes, whole_reply)
 		acc.AddFields(s, map[string]interface{}{"value": 1}, nil)
 	}
 	return nil
@@ -36,10 +50,4 @@ func (s *Sensor) Gather(acc telegraf.Accumulator) error {
 
 func init() {
 	inputs.Add("simple", func() telegraf.Input { return &Sensor{} })
-	c := &serial.Config{Name: "/dev/tty.usbmodem14101", Baud: 9600, ReadTimeout: 1000}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		log.Printf("cannot open serial: %s\n", err)
-		return "null"
-	}
 }
